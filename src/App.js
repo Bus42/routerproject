e9487b1e22f2eaa4282c73c0a6271b8e5cff0b9e
Greg Brewton <gbrewton1980@gmail.com>
2021-04-05 20:38:52 -0600

    Populates friends with mock API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Home = () => (
   <div id="home">
@@ -7,11 +8,30 @@ const Home = () => (
   </div>
 );
 
-const Friends = () => (
+const Friends = () => {
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    fetch('friendslist.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(res => setFriends(res.data))
+  }, [])
+
+  return (
   <div id="friends">
     <h2>Friends Component</h2>
+    {friends.map((friend, index) => (
+      <div className="friend" key={index} >
+        <h3>{friend.firstName} {friend.lastName}</h3>
+        <p>Hometown: {friend.hometown}</p>
+      </div>
+    ))}
   </div>
-);
+)};
 
 function App() {
   return (
@@ -19,16 +39,16 @@ function App() {
       <Router>
         <nav className="navbar">
           <li>
-            <Link to='/' >Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to='friends' >Friends</Link>
+            <Link to="friends">Friends</Link>
           </li>
         </nav>
         <div className="app">
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/friends' component={Friends} />
+            <Route exact path="/" component={Home} />
+            <Route path="/friends" component={Friends} />
           </Switch>
         </div>
       </Router>
