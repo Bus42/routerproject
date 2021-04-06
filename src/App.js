3b475ad69e49c6ba0fc0b33ca01690170389cd8d
Greg Brewton <gbrewton1980@gmail.com>
2021-04-05 21:10:15 -0600

    Uses avengersData for friends, adds detail

--- src/App.js
+++ src/App.js
@@ -11,27 +11,54 @@ const Home = () => (
 const Friends = () => {
   const [friends, setFriends] = useState([]);
   useEffect(() => {
-    fetch('friendslist.json', {
+    fetch("friendslist.json", {
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      }
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
-    .then(res => res.json())
-    .then(res => setFriends(res.data))
-  }, [])
+      .then((res) => res.json())
+      .then((res) => setFriends(res.data));
+  }, []);
+
+  const Friend = ({ name, thumbnail }) => {
+    return (
+      <div className="friend">
+        <img src={thumbnail} alt={name} />
+        <h3>{name}</h3>
+      </div>
+    );
+  };
+
+  const FriendDetail = ({ id, name, nickname, description, img, movies }) => {
+    return (
+      <div className="friendDetail">
+        <h2>
+          {name}, aka {nickname}
+        </h2>
+        <img src={img} alt={name} />
+        <p>{description}</p>
+        <ul>
+          Movies
+          {movies.map((movie, index) => (
+            <li className="movie" key={`${id}_${index}`}>
+              {movie}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
 
   return (
-  <div id="friends">
-    <h2>Friends Component</h2>
-    {friends.map((friend, index) => (
-      <div className="friend" key={index} >
-        <h3>{friend.firstName} {friend.lastName}</h3>
-        <p>Hometown: {friend.hometown}</p>
-      </div>
-    ))}
-  </div>
-)};
+    <div id="friends">
+      <h2>Friends Component</h2>
+      {friends.map((friend, index) => (
+        <Friend key={index} name={friend.name} thumbnail={friend.thumbnail} />
+      ))}
+    </div>
+  );
+};
 
 function App() {
   return (
